e36adbaeb9518d00e06200445cdd4c2d74353153
Paul Tran <paultran0307@gmail.com>
2022-01-23 13:30:51 -0500

    First lesson
    first lesson

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'mw-app',
+    templateUrl: './app.component.html',
+    // styles : [`
+    //     h1 {
+    //         color: white;
+    //     }
+    // `
+    // ]
+    styleUrls : ['./app.component.css']
+})
+export class AppComponent {}
